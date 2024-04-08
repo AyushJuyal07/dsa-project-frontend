18eeaf44f45939f979253f5fe7b794f889e841d5
Ayush Juyal <ayushjuyal26@gmail.com>
2024-04-09 01:17:09 +0530

    created offcampus and contact us page

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,10 +1,40 @@
-import React from 'react'
-import "./Contact.css"
+import React from "react";
+import "./Contact.css";
 
 function Contact() {
   return (
-    <div className='contact'>Contact</div>
-  )
+    <div className="container">
+      <div className="row input-container">
+        <div className="col-xs-12">
+          <div className="styled-input wide">
+            <input type="text" required />
+            <label>Name</label>
+          </div>
+        </div>
+        <div className="col-md-6 col-sm-12">
+          <div className="styled-input">
+            <input type="text" required />
+            <label>Email</label>
+          </div>
+        </div>
+        <div className="col-md-6 col-sm-12">
+          <div className="styled-input" style={{float: "right"}}>
+            <input type="text" required />
+            <label>Phone Number</label>
+          </div>
+        </div>
+        <div className="col-xs-12">
+          <div className="styled-input wide">
+            <input required />
+            <label>Message</label>
+          </div>
+        </div>
+        <div className="col-xs-12 sbt-button">
+          <div className="btn-lrg submit-btn">Send Message</div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Contact
+export default Contact;
--- src/components/OffCampus/OffCampus.jsx
+++ src/components/OffCampus/OffCampus.jsx
@@ -1,9 +1,58 @@
 import React from 'react'
 import "./OffCampus.css"
+import img from '../../assets/images.png'
 
 function OffCampus() {
   return (
-    <div className='offcampus'>OffCampus</div>
+    <div className='offcampus'>
+      <div className='card'>
+        <div className='card-img'><img src={img} alt="" /></div>
+        <div className='card-text'> 
+          <h1>Course name</h1>
+          <button>Start</button>
+        </div>
+      </div>
+
+      <div className='card'>
+        <div className='card-img'><img src={img} alt="" /></div>
+        <div className='card-text'> 
+          <h1>Course name</h1>
+          <button>Start</button>
+        </div>
+      </div>
+
+      <div className='card'>
+        <div className='card-img'><img src={img} alt="" /></div>
+        <div className='card-text'> 
+          <h1>Course name</h1>
+          <button>Start</button>
+        </div>
+      </div>
+
+      <div className='card'>
+        <div className='card-img'><img src={img} alt="" /></div>
+        <div className='card-text'> 
+          <h1>Course name</h1>
+          <button>Start</button>
+        </div>
+      </div>
+
+      <div className='card'>
+        <div className='card-img'><img src={img} alt="" /></div>
+        <div className='card-text'> 
+          <h1>Course name</h1>
+          <button>Start</button>
+        </div>
+      </div>
+
+      <div className='card'>
+        <div className='card-img'><img src={img} alt="" /></div>
+        <div className='card-text'> 
+          <h1>Course name</h1>
+          <button>Start</button>
+        </div>
+      </div>
+    </div>
   )
 }
 
